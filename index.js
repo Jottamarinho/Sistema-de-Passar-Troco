90898a5efe523c5da61dee14c5f58b29079acc90
Jotta <donsr922@gmail.com>
2023-10-16 15:50:28 -0300

    gear V

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function calcularTroco() {
+    const valorPago = parseFloat(document.getElementById('pago').value);
+    const totalCompra = parseFloat(document.getElementById('total').value);
+    const troco = valorPago - totalCompra;
+
+    if (troco >= 0) {
+        document.getElementById('troco').textContent = `Troco: R$ ${troco.toFixed(2)}`;
+    } else {
+        document.getElementById('troco').textContent = 'Valor insuficiente.';
+    }
+}
